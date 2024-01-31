2d40e37bae4dcb8b70b45cb81f75ed5ec05f722f
mwg396 <153116787+mwg396@users.noreply.github.com>
2024-01-31 08:09:58 -0600

    Update apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -15,38 +15,23 @@ module.exports = function (app) {
 			if (err) throw err;
             res.json(database);
 		});
-	});
+
 
     app.get('/api/notes', function (req, res) {
 		res.json(database);
 	});
 
 
-    app.delete('/api/notes/:id', (req, res) => {
-        const noteId = req.params.id; 
+    app.delete('/api/notes', (req, res) => {
 
-        fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
-          if (err) {
-            console.error(err);
-            return res.status(500).json({ error: 'Failed to delete note.' });
-          }
-      
-          let notes = JSON.parse(database); 
-          const noteIndex = notes.findIndex((database) => note.id === noteId);
-      
-          if (noteIndex === -1) {
-            return res.status(404).json({ error: 'Note not found.' });
-          }
       
           notes.splice(noteIndex, 1);
       
-          fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(database), (err) => {
-            if (err) {
-              console.error(err);
-              return res.status(500).json({ error: 'Failed to delete note.' });
-            }
-      
-            res.json({ message: 'Note deleted successfully.' });
+          fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(database), function (err) {
+            if (err) throw err;
+              return res.json(database);
+            });
+    
           });
         });
-      })};
+      };
